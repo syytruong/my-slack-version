b8e2d59e6cebd375b885f4f65192017b79b5cb0e
syytruong <syytruong@gmail.com>
2021-03-21 03:19:52 +0200

    implement header and sidebar fe side

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyBkPJUAYuYzERVwpkNboXpPuGrA88S2PVs',
+  authDomain: 'my-slack-version.firebaseapp.com',
+  projectId: 'my-slack-version',
+  storageBucket: 'my-slack-version.appspot.com',
+  messagingSenderId: '243822036198',
+  appId: '1:243822036198:web:6f94d399283cec7bfce727',
+  measurementId: 'G-RKLLRJ2EV3',
+};
